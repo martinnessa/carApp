5992f400f2415d9531409a2842f620181301450d
Martin Nessa <martinnessa7@gmail.com>
2024-03-07 13:27:40 +0100

    initial commit

---
+++ src/main/java/com/madsennessa/carprojectapp/CarProjectAppApplication.java
@@ -0,0 +1,13 @@
+package com.madsennessa.carprojectapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarProjectAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarProjectAppApplication.class, args);
+    }
+
+}
