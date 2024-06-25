fda489645b38944dcf1eff4778a487b765d4446d
Martin Nessa <martinnessa7@gmail.com>
2024-06-25 21:09:33 +0200

    More stuff

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,17 +1,11 @@
 import './tailwind.css'
+import {Navbar} from "./components/navbar.tsx";
 
 function App() {
 
   return (
     <>
-        <div className="grid grid-cols-2 h-12">
-            <div className="col-span-1 text-left pt-1.5 pl-1.5">
-                <p>CarApp</p>
-            </div>
-            <div className="col-span-1">
-
-            </div>
-        </div>
+        <Navbar/>
     </>
   )
 }
---
+++ react/src/components/navbar.tsx
@@ -0,0 +1,12 @@
+export function Navbar ()  {
+    return <>
+        <div className="bg-gray-600 grid grid-cols-2 h-12">
+            <div className="col-span-1">
+                <p className="text-left pt-2.5 pl-2.5">CarApp</p>
+            </div>
+            <div className="col-span-1">
+                <p className="text-right pt-2.5 pr-2.5">Account Settings</p>
+            </div>
+        </div>
+    </>
+}
